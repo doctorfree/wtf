a105372e7ff72efeab172f4ee7bf6286b9225e88
Ronald Record <github@ronrecord.com>
2022-12-24 11:17:01 -0800

    Model after ipapi module, bump release

--- modules/pokemon/settings.go
+++ modules/pokemon/settings.go
@@ -10,11 +10,19 @@ const (
 	defaultTitle     = "Pokémon"
 )
 
+type colors struct {
+	name  string
+	value string
+}
+
 type Settings struct {
 	*cfg.Common
 
+	colors
 	id       int
 	random   bool
+	language string
+	args []interface{} `help:"Defines what data to display and the order." values:"'height', 'weight', 'name', 'genus', and/or 'id'"`
 }
 
 func NewSettingsFromYAML(name string, ymlConfig *config.Config, globalConfig *config.Config) *Settings {
@@ -23,7 +31,12 @@ func NewSettingsFromYAML(name string, ymlConfig *config.Config, globalConfig *co
 
 		id:       ymlConfig.UInt("id", 0),
 		random:   ymlConfig.UBool("random", true),
+		language: ymlConfig.UString("language", "en"),
+		args:     ymlConfig.UList("args"),
 	}
+
+	settings.colors.name = ymlConfig.UString("colors.name", "red")
+	settings.colors.value = ymlConfig.UString("colors.value", "white")
 
 	settings.SetDocumentationPath("pokemon")
 
--- modules/pokemon/widget.go
+++ modules/pokemon/widget.go
@@ -1,16 +1,18 @@
 package pokemon
 
 import (
-	"io"
+	"bytes"
+    "encoding/json"
+    "fmt"
 	"math/rand"
 	"net/http"
 	"strconv"
 	"strings"
+	"text/template"
 	"time"
 
-//	"github.com/mtslzr/pokeapi-go"
-//	"github.com/mtslzr/pokeapi-go/structs"
 	"github.com/rivo/tview"
+	"github.com/doctorfree/wtf/utils"
 	"github.com/doctorfree/wtf/view"
 	"github.com/doctorfree/wtf/wtf"
 )
@@ -22,12 +24,22 @@ type Widget struct {
 	settings *Settings
 }
 
+var argLookup = map[string]string {
+	"id":            "Species ID",
+	"name":          "Species Name",
+	"genus":         "Species Genus",
+	"height":        "Height (m)",
+	"weight":        "Weight (kg)",
+}
+
 func NewWidget(tviewApp *tview.Application, redrawChan chan bool, settings *Settings) *Widget {
 	widget := Widget{
 		TextWidget: view.NewTextWidget(tviewApp, redrawChan, nil, settings.Common),
 
 		settings: settings,
 	}
+
+	widget.View.SetWrap(false)
 
 	return &widget
 }
@@ -57,20 +69,117 @@ func (widget *Widget) pokemon() {
 
 	req.Header.Set("User-Agent", "curl")
 	response, err := client.Do(req)
-//	var response structs.Pokemon
 
-//	response, err := pokeapi.Pokemon(strconv.Itoa(id))
 	if err != nil {
 		widget.result = err.Error()
 		return
 	}
 	defer func() { _ = response.Body.Close() }()
 
-	contents, err := io.ReadAll(response.Body)
+    var pokemonObject Pokemon
+	err = json.NewDecoder(response.Body).Decode(&pokemonObject)
 	if err != nil {
 		widget.result = err.Error()
 		return
 	}
 
-	widget.result = strings.TrimSpace(wtf.ASCIItoTviewColors(string(contents)))
+	spreq, err := http.NewRequest("GET", pokemonObject.Species.URL, http.NoBody)
+	if err != nil {
+		widget.result = err.Error()
+		return
+	}
+
+	spreq.Header.Set("User-Agent", "curl")
+	spresponse, err := client.Do(spreq)
+
+	if err != nil {
+		widget.result = err.Error()
+		return
+	}
+	defer func() { _ = spresponse.Body.Close() }()
+
+    var speciesObject PokemonSpecies
+	err = json.NewDecoder(spresponse.Body).Decode(&speciesObject)
+	if err != nil {
+		widget.result = err.Error()
+		return
+	}
+
+	widget.setResult(&pokemonObject, &speciesObject)
+}
+
+func (widget *Widget) setResult(poke *Pokemon, spec *PokemonSpecies) {
+
+	args := utils.ToStrs(widget.settings.args)
+
+	// if no arguments are defined set default
+	if len(args) == 0 {
+		args = []string{"id", "name", "height", "weight", "genus"}
+	}
+
+	format := ""
+
+	for _, arg := range args {
+		if val, ok := argLookup[strings.ToLower(arg)]; ok {
+			format = format + formatableText(val, strings.ToLower(arg))
+		}
+	}
+
+	resultTemplate, _ := template.New("pokemon_result").Parse(format)
+
+	langconfig := widget.settings.language
+	resultBuffer := new(bytes.Buffer)
+	pokemon_name := "Unknown"
+	for i := range spec.Names {
+        if spec.Names[i].Language.Name == langconfig {
+			pokemon_name = spec.Names[i].Name
+        }
+    }
+	if pokemon_name == "Unknown" {
+		langconfig = "en"
+	    for i := range spec.Names {
+            if spec.Names[i].Language.Name == langconfig {
+			    pokemon_name = spec.Names[i].Name
+            }
+        }
+	}
+
+	langconfig = widget.settings.language
+	pokemon_genus := "Unknown"
+	for i := range spec.Genera {
+        if spec.Genera[i].Language.Name == langconfig {
+			pokemon_genus = spec.Genera[i].Genus
+        }
+    }
+	if pokemon_genus == "Unknown" {
+		langconfig = "en"
+	    for i := range spec.Genera {
+            if spec.Genera[i].Language.Name == langconfig {
+			    pokemon_genus = spec.Genera[i].Genus
+            }
+        }
+	}
+
+	err := resultTemplate.Execute(resultBuffer, map[string]string{
+		"nameColor":     widget.settings.colors.name,
+		"valueColor":    widget.settings.colors.value,
+		"id":            strconv.Itoa(spec.ID),
+		"name":          pokemon_name,
+		"genus":         pokemon_genus,
+		"height":        strconv.Itoa(poke.Height),
+		"weight":        strconv.Itoa(poke.Weight),
+	})
+
+	if err != nil {
+		widget.result = err.Error()
+	}
+
+//	idstr := strconv.Itoa(pokemon_id)
+//  fmt.Println("▐[1;7m No. " + idstr + "[0m▌ [1m" + pokemon_name + " - " + pokemon_genus + "[0m")
+
+	widget.result = resultBuffer.String()
+}
+
+func formatableText(key, value string) string {
+	return fmt.Sprintf(" [{{.nameColor}}]%s: [{{.valueColor}}]{{.%s}}\n", key, value)
 }
